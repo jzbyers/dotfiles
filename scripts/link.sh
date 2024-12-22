10abe30ff81ed3dea7a24b76d531591cc731f15e
Jack Byers <jzbyers@gmail.com>
2024-12-21 22:27:42 -0500

    links

--- scripts/link.sh
+++ scripts/link.sh
@@ -5,8 +5,8 @@ ln -sF ~/src/github.com/jzbyers/dotfiles/kitty ~/.config/kitty
 ln -sF ~/src/github.com/jzbyers/dotfiles/alacritty ~/.config/alacritty
 ln -sF ~/src/github.com/jzbyers/dotfiles/nvim ~/.config/nvim
 ln -sF ~/src/github.com/jzbyers/dotfiles/bat ~/.config/bat
-ln -sF ~/src/github.com/jzbyers/dotfiles/tmuxconf ~/.tmux.conf
-ln -sF ~/src/github.com/jzbyers/dotfiles/psqlrc ~/.psqlrc
+ln -sF ~/src/github.com/jzbyers/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sF ~/src/github.com/jzbyers/dotfiles/.psqlrc ~/.psqlrc
 ln -sF ~/src/github.com/jzbyers/dotfiles/.gitconfig ~/.gitconfig
 ln -sF ~/src/github.com/jzbyers/dotfiles/.githelpers ~/.githelpers
 ln -sF ~/src/github.com/jzbyers/dotfiles/ideavimrc ~/.ideavimrc
